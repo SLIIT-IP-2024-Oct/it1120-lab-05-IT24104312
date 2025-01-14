e6f73d6d46ad72f6e7fe6c60f04deb795afb3f7e
IT24104312 <IT24104312@my.sliit.lk>
2025-01-14 21:02:16 +0530

    adding Q1

---
+++ Q1/IT24104312Lab5Q1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class IT24104312Lab5Q1{
+ public static void main(String[] args){
+  Scanner scanner=new Scanner(System.in);
+
+  System.out.print("Enter the first integer:");
+  int num1=scanner.nextInt();
+  System.out.print("Enter the second integer:");
+  int num2=scanner.nextInt();
+  System.out.print("Enter the third integer:");
+  int num3=scanner.nextInt();
+  int largest=(num1>num2)?(num1>num3?num1:num3):(num2>num3?num2:num3);
+  int smallest=(num1<num2)?(num1<num3?num1:num3):(num2<num3?num2:num3);
+  System.out.println("User entered numbers are:"+num1+" "+num2+" "+num3);
+  System.out.println("The Smallest number is:"+smallest);
+  System.out.println("The largest number is:"+largest);
+ }
+}
+
+
+  
+
+
